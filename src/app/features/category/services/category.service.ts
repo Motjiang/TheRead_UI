00faf323f2d176fb1aec2c22f02d4ce1d85a88f6
Motjiang <motjiangbrilliant@gmail.com>
2024-09-18 00:08:31 +0200

    Added the subscription method and the Ondestroy interface

--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AddCategoryRequest } from '../models/add-category-request.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  addCategory(model: AddCategoryRequest): Observable<void>{
+
+    return this.http.post<void>('https://localhost:7247/api/categories',model);
+
+  }
+
+   
 }
