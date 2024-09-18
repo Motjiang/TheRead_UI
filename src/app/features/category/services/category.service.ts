9643a2a67b9c5b6508df36b3636ecde76333a71a
Motjiang <motjiangbrilliant@gmail.com>
2024-09-18 05:11:32 +0200

    Changed the methods to consume the recent created variable to use the API

--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { Observable } from 'rxjs';
 import { Category } from '../models/category.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +14,10 @@ export class CategoryService {
 
   addCategory(model: AddCategoryRequest): Observable<void>{
 
-    return this.http.post<void>('https://localhost:7247/api/categories',model);
+    return this.http.post<void>(`${environment.apiBaseUrl}/api/categories`,model);
   }
 
   getAllCategories():Observable<Category[]> {
-    return this.http.get<Category[]>('https://localhost:7247/api/categories');
+    return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/categories`);
   } 
 }
