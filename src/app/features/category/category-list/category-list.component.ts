094d1f1ae75489c46b26e5248614fdcbe1479396
Motjiang <motjiangbrilliant@gmail.com>
2024-09-18 05:19:34 +0200

    Added the getAll method

--- src/app/features/category/category-list/category-list.component.ts
+++ src/app/features/category/category-list/category-list.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Category } from '../models/category.model';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent {
+export class CategoryListComponent  implements OnInit{
+
+  categories?: Category[];
+
+
+  constructor(private categotyService: CategoryService){
+
+  }
+
+  ngOnInit(): void {
+    this.categotyService.getAllCategories().subscribe({
+      next:(response) => {
+        this.categories = response
+      }
+    })
+  }
+
+  
 
 }
