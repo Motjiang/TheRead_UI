425021444f5a536d2c01829918431a281c74a8dd
Motjiang <motjiangbrilliant@gmail.com>
2024-09-18 05:55:20 +0200

    Added the edit category component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,8 @@ import { NavbarComponent } from './core/components/navbar/navbar.component';
 import { CategoryListComponent } from './features/category/category-list/category-list.component';
 import { AddCategoryComponent } from './features/category/add-category/add-category.component';
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from  '@angular/common/http'
+import {HttpClientModule} from  '@angular/common/http';
+import { EditCategoryComponent } from './features/category/edit-category/edit-category.component'
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import {HttpClientModule} from  '@angular/common/http'
     AppComponent,
     NavbarComponent,
     CategoryListComponent,
-    AddCategoryComponent
+    AddCategoryComponent,
+    EditCategoryComponent
   ],
   imports: [
     BrowserModule,
