83f5977d87742a80a3ebde93f3ec957dc18b465e
Motjiang <motjiangbrilliant@gmail.com>
2024-09-19 03:10:58 +0200

    Remove the update category methods

--- src/app/features/category/edit-category/edit-category.component.ts
+++ src/app/features/category/edit-category/edit-category.component.ts
@@ -3,7 +3,6 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { CategoryService } from '../services/category.service';
 import { Category } from '../models/category.model';
-import { UpdateCategoryRequest } from '../models/update-category-request.model';
 
 @Component({
   selector: 'app-edit-category',
@@ -41,36 +40,9 @@ export class EditCategoryComponent implements OnInit, OnDestroy {
     });
   }
 
-  onFormSubmit(): void {
-    const updateCategoryRequest: UpdateCategoryRequest = {
-      name: this.category?.name ?? '',
-      urlHandle: this.category?.urlHandle ?? ''
-    };
-
-    // pass this object to service
-    if (this.id) {
-      this.editCategorySubscription = this.categoryService.updateCategory(this.id, updateCategoryRequest)
-      .subscribe({
-        next: (response) => {
-          this.router.navigateByUrl('/admin/categories');
-        }
-      });
-    }
-  }
-
-  // onDelete(): void {
-  //   if (this.id) {
-  //     this.categoryService.deleteCategory(this.id)
-  //     .subscribe({
-  //       next: (response) => {
-  //         this.router.navigateByUrl('/admin/categories');
-  //       }
-  //     })
-  //   }
-  // }
 
   ngOnDestroy(): void {
     this.paramsSubscription?.unsubscribe();
-    this.editCategorySubscription?.unsubscribe();
+    
   }
 }
