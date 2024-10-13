cf3fccd91cfce888b78fcf2177b746badecd3e68
Motjiang <motjiangbrilliant@gmail.com>
2024-10-13 21:40:50 +0200

    Added an Add-blogPost interface model

---
+++ src/app/features/blog-post/models/add-blog-post.model.ts
@@ -0,0 +1,10 @@
+export interface AddBlogPost{
+    title: string;
+    shshortDescription: string;
+    content:string;
+    featuredImageUrl:string;
+    urlHandle:string;
+    author:string;
+    publishedDate: Date;
+    isVisible: boolean;
+}
